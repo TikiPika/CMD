f20dee42a351bcc01430ce91bc721cee4652e23f
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-04 11:16:09 +0800

    Update ChangeDns.bat

--- ChangeDns.bat
+++ ChangeDns.bat
@@ -2,9 +2,12 @@
 
 rem 获取当前网络连接接口的 GUID
 for /f "tokens=3 delims=\" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /s /f "IPAddress"') do (
-    set "interfaceGuid=%%a"
-    goto :foundInterfaceGuid
+    for /f "tokens=2 delims=:" %%i in ('netsh interface ip show address "%%a" ^| findstr /c:"Address"') do (
+        set "ip=%%i"
+        if "!ip:~0,8!"=="172.24.2" (
+            rem 在注册表中设置 DNS 地址
+            reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\%%a" /v "NameServer" /t REG_SZ /d "8.8.8.8,8.8.4.4,1.1.1.1,1.0.0.1" /f
+            echo 已将 DNS 地址更新为 8.8.8.8, 8.8.4.4, 1.1.1.1, 1.0.0.1。
+        )
+    )
 )
-:foundInterfaceGuid
-
-echo 当前网络连接接口的 GUID 为: %interfaceGuid%
