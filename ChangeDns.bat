70e95e06b963c9383cd23fac5afe703675bfade0
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-04 11:08:52 +0800

    Update ChangeDns.bat

--- ChangeDns.bat
+++ ChangeDns.bat
@@ -1,8 +1,10 @@
 @echo off
 
-rem 获取当前网络连接名称
-for /f "tokens=3 delims=\" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /s /f "DhcpIPAddress"') do (
-    set "connectionName=%%a"
+rem 获取当前网络连接接口的 GUID
+for /f "tokens=3 delims=\" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /s /f "IPAddress"') do (
+    set "interfaceGuid=%%a"
+    goto :foundInterfaceGuid
 )
+:foundInterfaceGuid
 
-echo 当前网络连接名称为: %connectionName%
+echo 当前网络连接接口的 GUID 为: %interfaceGuid%
