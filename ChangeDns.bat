04fa4376fb511e854a4957d5a001cd3a57d91377
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-04 11:02:49 +0800

    Update ChangeDns.bat

--- ChangeDns.bat
+++ ChangeDns.bat
@@ -1,16 +1,8 @@
 @echo off
-setlocal enabledelayedexpansion
 
 rem 获取当前网络连接名称
-for /f "tokens=* delims=" %a in ('netsh interface show interface') do (
-    set "connectionName=%a"
-)
-
-rem 提取最后一行非空数据作为当前网络连接名称
-for /f "tokens=* delims=" %b in ("%connectionName%") do (
-    if not "%b"=="" (
-        set "connectionName=%b"
-    )
+for /f "tokens=3 delims=\" %%a in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces" /s /f "DhcpIPAddress"') do (
+    set "connectionName=%%a"
 )
 
 echo 当前网络连接名称为: %connectionName%
