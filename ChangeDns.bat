e6fcefff447c636f2c193a8c34c1a54aa08660fa
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-04 10:05:33 +0800

    Update ChangeDns.bat

--- ChangeDns.bat
+++ ChangeDns.bat
@@ -1,20 +1,24 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-REM 替换为你的DNS服务器地址
-set primaryDNS=8.8.8.8
-set secondaryDNS=8.8.4.4
-set tertiaryDNS=1.1.1.1
-set quaternaryDNS=1.0.0.1
+rem 设置要使用的 DNS 地址
+set dns1=8.8.8.8
+set dns2=8.8.4.4
+set dns3=1.1.1.1
+set dns4=1.0.0.1
 
-REM 获取所有启用的网络适配器名称
-for /f "tokens=*" %%A in ('wmic nic where "NetEnabled=true" get NetConnectionID ^| findstr /r /v "^$" ^| findstr /v "NetConnectionID"') do (
-    REM 设置DNS服务器地址
-    netsh interface ipv4 set dns name="%%A" static %primaryDNS%
-    netsh interface ipv4 add dns name="%%A" addr=%secondaryDNS% index=2
-    netsh interface ipv4 add dns name="%%A" addr=%tertiaryDNS% index=3
-    netsh interface ipv4 add dns name="%%A" addr=%quaternaryDNS% index=4
+rem 获取当前的连接名称
+for /f "tokens=*" %%a in ('netsh interface ipv4 show interfaces ^| findstr /R /C:"[0-9]"') do (
+    set connectionName=%%a
+    goto :continue
 )
 
-endlocal
-pause
+:continue
+
+rem 设置 DNS 地址
+netsh interface ipv4 set dns name="!connectionName!" source=static addr=%dns1%
+netsh interface ipv4 add dns name="!connectionName!" addr=%dns2% index=2
+netsh interface ipv4 add dns name="!connectionName!" addr=%dns3% index=3
+netsh interface ipv4 add dns name="!connectionName!" addr=%dns4% index=4
+
+echo DNS 地址已经更新为 %dns1%, %dns2%, %dns3%, %dns4%
