fb8d7253b912be51c271d013b41420902d30bd1c
TikiPika <77382240+TikiPika@users.noreply.github.com>
2024-06-04 10:59:51 +0800

    Update ChangeDns.bat

--- ChangeDns.bat
+++ ChangeDns.bat
@@ -2,15 +2,15 @@
 setlocal enabledelayedexpansion
 
 rem 获取当前网络连接名称
-for /f "tokens=* delims=" %%a in ('netsh interface show interface') do (
-    set "connectionName=%%a"
+for /f "tokens=* delims=" %a in ('netsh interface show interface') do (
+    set "connectionName=%a"
 )
 
 rem 提取最后一行非空数据作为当前网络连接名称
-for /f "tokens=* delims=" %%b in ("!connectionName!") do (
-    if not "%%b"=="" (
-        set "connectionName=%%b"
+for /f "tokens=* delims=" %b in ("%connectionName%") do (
+    if not "%b"=="" (
+        set "connectionName=%b"
     )
 )
 
-echo 当前网络连接名称为: !connectionName!
+echo 当前网络连接名称为: %connectionName%
